c94c5b3f0c743f4c478a2728148f7984df88ace0
Abdoul Diallo <ard@Abdouls-MacBook-Pro.local>
2024-06-13 17:57:50 +0300

    add routes

--- React-Jobs/src/App.jsx
+++ React-Jobs/src/App.jsx
@@ -1,19 +1,30 @@
+import {
+  Route,
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";
 import JobListings from "./components/JobListings";
 import ViewAllJobsButton from "./components/ViewAllJobsButton";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+import NotFound from "./pages/NotFound";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
 const App = () => {
-	return (
-		<>
-			<Navbar />
-			<Hero />
-			<HomeCards />
-			<JobListings />
-			<ViewAllJobsButton />
-		</>
-	);
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ React-Jobs/src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import Hero from "../components/Hero";
+import HomeCards from "../components/HomeCards";
+import JobListings from "../components/JobListings";
+import ViewAllJobsButton from "../components/ViewAllJobsButton";
+
+const HomePage = () => {
+  return (
+    <>
+      <Hero />
+      <HomeCards />
+      <JobListings isHome={true} />
+      <ViewAllJobsButton />
+    </>
+  );
+};
+
+export default HomePage;
